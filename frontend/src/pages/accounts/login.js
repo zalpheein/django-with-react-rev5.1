e6bf8570091e5fd3ebfb9f10495eabeb0010929c
zalphee <zlaphee@gmail.com>
2022-07-01 21:08:34 +0900

    frontend 폴더 전체 삭제... 새로이 만들 예정

--- frontend/src/pages/accounts/login.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-
-function Login() {
-    return <div>accounts/Login</div>;
-}
-
-export default Login;
--- frontend/src/pages/accounts/profile.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-
-function Profile() {
-    return <div>accounts/Profile</div>;
-}
-
-export default Profile;
