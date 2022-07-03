d772e8eb8fee51362f74b31db257e4c1c70892b7
zalphee <zlaphee@gmail.com>
2022-07-03 17:32:49 +0900

    django 패키지 버전 확인

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
+from rest_framework_jwt.views import obtain_jwt_token
 from . import views
 
 
 urlpatterns = [
     path('signup/', views.SignupView.as_view(), name='login'),
+    path('token/', obtain_jwt_token),
 ]
--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -140,9 +140,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # 레스트 프레임워크 퍼미션 인증을 정의 하여 
 # 로그인 사용자만 데이터에 접근하도록 제한
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES' : [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
+    'DEFAULT_PERMISSION_CLASSES' : ['rest_framework.permissions.IsAuthenticated',],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+            'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+            'rest_framework.authentication.SessionAuthentication',
+    ],
 }
 
 
